5aee955cd5716bd72ee07dfe896dc5d3dceed21c
RUSHHE <2433323047@qq.com>
2023-09-20 11:01:47 +0800

    给地雷添加了图标

--- src/MinesweeperButton.java
+++ src/MinesweeperButton.java
@@ -1,14 +1,53 @@
 import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class MinesweeperButton extends JButton {
     private final int status; // -1是雷，0-8为附近的雷数
     private final int xLocation;
     private final int yLocation;
+    MouseListener mouseListener = new MouseListener() {
+        @Override
+        public void mouseClicked(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+            //左键点击
+            if (e.getButton() == MouseEvent.BUTTON1) {
+                if (status == -1) {
+                    setIcon(new ImageIcon("./src/Themes/Classic/Button_Mine_exploded.png"));
+                } else {
+                    setIcon(new ImageIcon("./src/Themes/Classic/Button_" + status + ".png"));
+                }
+            } else if (e.getButton() == MouseEvent.BUTTON3) {
+                setIcon(new ImageIcon("./src/Themes/Classic/Button_flag.png"));
+            }
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+
+        }
+    };
     private boolean minesVisible = false;//false为不可见，true为可见
 
     public MinesweeperButton(int xLocation, int yLocation, boolean[][] status) {
         //调用JButton类的无参构造方法，创建一个没有文本或图标的按钮
         super();
+
+        setIcon(new ImageIcon("./src/Themes/Classic/Button.png"));
         this.xLocation = xLocation;
         this.yLocation = yLocation;
         if (status[yLocation][xLocation]) {
@@ -17,15 +56,7 @@ public class MinesweeperButton extends JButton {
             this.status = calculateNearbyMines(status);
         }
         //添加按钮的点击事件，可以根据自己的逻辑来实现
-        this.addActionListener(e -> {
-            //TODO: 按钮被点击时要执行的操作,如果是雷就全部爆炸，如果为空，如果周围有雷，显示雷的数量
-            if (this.status != -1) {
-                super.setText(String.valueOf(this.status));
-            } else {
-                super.setText("*");
-                super.setEnabled(false);
-            }
-        });
+        addMouseListener(mouseListener);
     }
 
     public void setMinesVisible(boolean minesVisible) {
@@ -45,4 +76,3 @@ public class MinesweeperButton extends JButton {
         return numbers;
     }
 }
-
